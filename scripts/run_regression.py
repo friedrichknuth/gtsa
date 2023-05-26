59d9d8bf4a390424e730aab17a5b668ef71531ca
Friedrich <knuth@uw.edu>
2023-05-26 12:36:32 -0700

    update regression script

--- scripts/run_regression.py
+++ scripts/run_regression.py
@@ -77,42 +77,6 @@ if __name__ == "__main__":
                                                            'prediction_time_series' : prediction_time_series})
             ds_result.to_zarr('rainier.zarr')
 
-#             xmin, ymin, xmax, ymax = ds_zarr['band1'].rio.bounds()
-#             bounds_gdf = gtsa.geospatial.bounds2polygon(xmin, xmax, ymin, ymax)
-#             glaciers_gdf = gpd.sjoin(rgi02_gdf, bounds_gdf, predicate='intersects') # intersecting glaciers
-#             glaciers_gdf = glaciers_gdf[glaciers_gdf['Name'].str.len() > 2] # named
-#             glaciers_gdf = glaciers_gdf[glaciers_gdf['Area']>1] # greater than 1 km^2
-            
-#             glaciers = glaciers_gdf['RGIId'].values
-
-#             for glacier in glaciers:
-#                 out = Path(output_directory, glacier).as_posix().replace('.','-')+'.zarr'
-
-#                 glacier_gdf = glaciers_gdf[glaciers_gdf['RGIId'].str.contains(glacier)]
-#                 try:
-#                     xmin, ymin, xmax, ymax = glacier_gdf.bounds.values[0]
-#                     subset = ds_zarr.sel(x=slice(xmin, xmax), y=slice(ymax,ymin))
-
-#                     times = [pd.to_datetime(i) for i in subset['band1'].time.values]
-#                     time_stamps = np.array([gtsa.utils.date_time_to_decyear(i) for i in times])
-#                     prediction_time_series = gtsa.temporal.create_prediction_timeseries(start_date = '1950-01-01',
-#                                                                                         end_date = '2020-01-01',
-#                                                                                         dt ='Y')
-#                     ds_result = gtsa.temporal.dask_apply_GPR(subset['band1'],
-#                                                            'time', 
-#                                                            kwargs={'times':time_stamps,
-#                                                                    'kernel': kernel,
-#                                                                    'prediction_time_series' : prediction_time_series})
-
-#                     ds_result.to_zarr(out)
-
-#                     print(out)
-#                 except:
-#                     print('NODATA',glacier)
-#                     pass
-
-
-
 
         t2 = time()
         print(f"Took {(t2-t1)/60:.2f} min to process with {workers} workers")
