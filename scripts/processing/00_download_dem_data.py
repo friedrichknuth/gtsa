ad33e53f1cafe481c94a0b4ac403e8b289a4b9be
Friedrich <knuth@uw.edu>
2023-07-20 17:38:13 -0700

    rename data download script

--- scripts/processing/00_download_dem_data.py
+++ scripts/processing/00_download_dem_data.py
@@ -1,12 +1,14 @@
 import gtsa
 
 sites = ['mount-baker', 'south-cascade']
-output_directory = '../test_data'
+output_directory = '../../data'
+product = 'dem'
 include_refdem = True
 overwrite = True
 
 for site in sites:
-    gtsa.dataquery.download_hi_res_test_data(site = site,
+    gtsa.dataquery.download_historical_data(site = site,
+                                             product = product,
                                              output_directory = output_directory,
                                              include_refdem = include_refdem,
                                              overwrite = overwrite,
