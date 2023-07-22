54d0da8328724eb1a04bd716754cb8c4832ff069
Friedrich <knuth@uw.edu>
2023-07-22 12:25:06 -0700

    update client

--- gtsa/cli/download_data.py
+++ gtsa/cli/download_data.py
@@ -1,56 +1,57 @@
 import click
 import gtsa
 
-@click.command()
+@click.command(
+    help='Downloads historical orthoimage and digital elevation model data from https://doi.org/10.5281/zenodo.7297154'
+)
+
 @click.option(
     "--site",
     prompt=True,
     default="south-cascade",
-    help="'mount-baker' or 'south-cascade'",
+    help="Use 'mount-baker' or 'south-cascade' (without quotes).",
 )
 
 @click.option(
     "--outdir",
     prompt=True,
     default="data",
-    help="output directory",
+    help="Your desired output directory path.",
 )
 
 @click.option(
     "--product",
     prompt=True,
     default="dem",
-    help="'dem' or 'ortho'",
+    help="Use 'dem' or 'ortho' (without quotes).",
 )
 
 @click.option(
     "--include_refdem",
     is_flag=True, 
     default=False, 
-    help="Set to download reference DEM for site"
+    help="Set to download reference DEM for site."
 )
 
 @click.option(
     "--max_workers", 
     default=None, 
-    help="Set to integer matching cores to be used"
+    help="Set to integer matching cores to be used."
 )
 
 @click.option(
     "--silent", 
     is_flag=True, 
     default=True, 
-    help="Set to silence printed information"
+    help="Set to silence information printed to stdout."
 )
 
 @click.option(
     "--overwrite", 
     is_flag=True, 
     default=False, 
-    help="Set to overwrite existing outputs"
+    help="Set to overwrite existing outputs."
 )
-
-
 
 def main(
     site,
